8261a5427a175f00554963fa01b1db4e8b21a331
clbokea <clbo@kea.dk>
2016-04-18 22:48:03 +0200

    initial

---
+++ appharborgithub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(appharborgithub.Startup))]
+namespace appharborgithub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
